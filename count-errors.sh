a73208bb20f8160144767757b452bd164c5b8e2b
Ricardo Lopez <ricardolopez93@gmail.com>
2015-03-20 18:05:54 -0400

    -

---
+++ count-errors.sh
@@ -0,0 +1,7 @@
+for file in $(ls genes/*err*)
+do
+    echo $file
+    echo "error reads: $(grep 'ERROR' $file -c)"
+    echo "correct reads: $(grep 'CORRECT' $file -c)"
+    echo "total reads: $(grep 'FBtr' $file -c)"
+done
